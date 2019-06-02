1be685850884d060c42285037f1d32caeca8c959
Max Gabb <mgabb2015@fau.edu>
2019-06-02 19:49:43 -0400

    tone tables scale to mobile

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -23,4 +23,16 @@ export class AnalysisComponent implements OnInit {
     this.tones = analysis.sentences_tone[0].tone_categories[0].tones;
   }
 
+  getClass(tone) {
+    let classes = {
+      'joy': tone.tone_name === 'Joy' && tone.score >= .5 && tone.score < .75,
+      'joyStrong': tone.tone_name === 'Joy' && tone.score >= .75,
+      'fear': tone.tone_name === 'Fear' && tone.score >= .5,
+      'sadness': tone.tone_name === 'Sadness' && tone.score >= .5,
+      'anger': tone.tone_name === 'Anger' && tone.score >= .5 && tone.score < .75,
+      'angerStrong': tone.tone_name === 'Anger' && tone.score >= .75
+    }
+    return classes;
+  }
+
 }
